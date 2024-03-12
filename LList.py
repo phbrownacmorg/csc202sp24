fc810545b7aba1abf961e312e5e6f147de318575
Peter Brown <phbrown@acm.org>
2024-03-12 11:28:49 -0400

    Added linked list (LList.py)

---
+++ LList.py
@@ -0,0 +1,108 @@
+from typing import cast, Generic, TypeVar
+
+T = TypeVar('T')
+
+class LList(Generic[T]):
+    """Class to represent a linked list.  In this implementation, a list
+    is either (1) an empty list, or (2) a node with data followed by
+    a linked list.  The empty list is represented by a sentinel node.
+    This list does not support storing None as a data value."""
+
+    def __invariant(self) -> bool:
+        """Class invariant."""
+        valid = True
+        if self.__next is None: # Sentinel node (empty list)
+            valid = self.__data is None
+        else: # There's more list after this node
+            valid = self.__data is not None and self.__next.__invariant()
+        return valid
+        
+    def __init__(self):
+        """Create an empty list."""
+        self.__data = None # Reference to data value
+        self.__next = None # Reference to next node
+        # Post:
+        assert self.__invariant()
+
+    # QUERY METHODS
+
+    def empty(self) -> bool:
+        """Returns True if the list is empty, else False."""
+        return self.__next is None # (and self.__data is None)
+    
+    def value(self) -> T:
+        """Returns the value stored in the current node."""
+        # Pre:
+        assert self.__invariant and not self.empty()
+        return self.__data
+    
+    def next(self) -> 'LList[T]':
+        """Returns the next node after this one."""
+        # Pre:
+        assert self.__invariant and not self.empty()
+        return self.__next
+    
+    def __len__(self) -> int:
+        """Returns the number of nodes in the list (not counting the sentinel)."""
+        nodes = -1
+        if self.empty(): # Empty list, or...
+            nodes = 0
+        else:            # A node followed by a linked list
+            nodes = 1 + len(self.next())
+        return nodes
+
+    def __str__(self) -> str:
+        """Returns a string representation of the list."""
+        result = ''
+        if self.empty(): # Empty list, or...
+            result = '∅'
+        else:            # A node followed by a linked list
+            result = f'❬{str(self.value())}❭➞' + str(self.next())
+        return result
+    
+    def index(self, val: T) -> int:
+        """Returns the index of the first occurrence of VAL in the list.
+        Raises ValueError if VAL is not in the list."""
+        idx = -1
+        if self.empty(): # Empty list, or...
+            raise ValueError(f"{val} is not in list")
+        elif self.value() == val: # A node...
+            idx = 0
+        else:            # followed by a list
+            idx = 1 + self.next().index(val)
+        return idx
+
+    # MUTATOR METHODS
+
+    def add(self, val: T) -> None:
+        """Add a node with value VAL at the beginning of the list."""
+        newnode = LList[T]()
+        newnode.__data = self.__data
+        newnode.__next = self.__next
+        self.__next = newnode
+        self.__data = val
+        # Post:
+        assert self.__data == val and self.__invariant()
+
+    def pop(self, idx: int = -1) -> T:
+        """Returns the item at index IDX, remiving it from the list."""
+        value: T | None = None
+        # Ensure IDX is within limits
+        size = len(self)
+        if idx < -size or idx >= size:
+            raise IndexError(f'List size is {size}; index {idx} out of range')
+
+        # Support negative indices (count from the end)
+        if idx < 0:
+            idx += size
+        assert self.empty() or idx >= 0, 'Index out of range not caught'
+        if self.empty(): # Empty list, or...
+            raise IndexError('Cannot pop from empty list')
+        elif idx == 0:   # a node...
+            value = self.value()
+            # Remove the item from the list.  Order is important!
+            self.__data = self.next().__data
+            self.__next = self.next().__next
+        else:            # followed by another node
+            value = self.next().pop(idx - 1)
+        return cast(T, value)
---
+++ TestLList.py
@@ -0,0 +1,119 @@
+import unittest
+# import the code you want to test here
+from LList import LList
+
+class TestLList(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.__empty = LList[int]() # empty list
+
+        self.__1 = LList[int]() 
+        self.__1.add(1)         # 1-node list
+
+        self.__4 = LList[int]()
+        self.__4.add(1)
+        self.__4.add(2)
+        self.__4.add(3)
+        self.__4.add(4)         # 4-node list
+
+    # Every method that starts with the string "test"
+    # will be executed as a unit test
+    def testEmptyTrue(self) -> None:
+        self.assertTrue(self.__empty.empty())
+
+    def testEmptyFalse(self) -> None:
+        self.assertFalse(self.__1.empty())
+        self.assertFalse(self.__4.empty())
+
+    def testValue(self) -> None:
+        self.assertEqual(self.__1.value(), 1)
+        self.assertEqual(self.__4.value(), 4)
+        with self.assertRaises(AssertionError):
+            self.__empty.value()
+
+    def testNext(self) -> None:
+        self.assertTrue(self.__1.next().empty())
+        self.assertTrue(self.__4.next().value(), 3)
+        self.assertTrue(self.__4.next().next().value(), 2)
+        self.assertTrue(self.__4.next().next().next().value(), 1)
+        with self.assertRaises(AssertionError):
+            self.__empty.next()
+
+    def testLen(self) -> None:
+        self.assertEqual(len(self.__empty), 0)
+        self.assertEqual(len(self.__1), 1)
+        self.assertEqual(len(self.__4), 4)
+
+    def testStr(self) -> None:
+        self.assertEqual(str(self.__empty), '∅')
+        self.assertEqual(str(self.__1), '❬1❭➞∅')
+        self.assertEqual(str(self.__4), '❬4❭➞❬3❭➞❬2❭➞❬1❭➞∅')
+
+    def testIndex(self) -> None:
+        with self.assertRaises(ValueError):
+            self.__empty.index(1)
+        self.assertEqual(self.__1.index(1), 0)
+        with self.assertRaises(ValueError):
+            self.__1.index(2)
+        for i in range(1, 5):
+            with self.subTest(i=i):
+                self.assertEqual(self.__4.index(i), 4-i)
+        with self.assertRaises(ValueError):
+            self.__1.index(5)
+        
+    def testPopEmpty(self) -> None:
+        with self.assertRaises(IndexError):
+            self.__empty.pop()
+
+    def testPop_1_1(self) -> None:
+        with self.assertRaises(IndexError):
+            self.__1.pop(1) # Too big
+
+    def testPop_1_n2(self) -> None:
+        with self.assertRaises(IndexError):
+            self.__1.pop(-2) # Too small
+
+    def testPop_1_0(self) -> None:
+        self.assertEqual(self.__1.pop(0), 1)
+        self.assertTrue(self.__1.empty())
+
+    def testPop_1_n1(self) -> None:
+        self.assertEqual(self.__1.pop(-1), 1)
+        self.assertTrue(self.__1.empty())
+
+    def testPop_4_4(self) -> None:
+        with self.assertRaises(IndexError):
+            self.__4.pop(4) # Too big
+
+    def testPop_4_n5(self) -> None:
+        with self.assertRaises(IndexError):
+            self.__4.pop(-5) # Too small
+
+    def testPop_4_0(self) -> None:
+        self.assertEqual(self.__4.pop(0), 4)
+        self.assertEqual(str(self.__4), '❬3❭➞❬2❭➞❬1❭➞∅')
+
+    def testPop_4_3(self) -> None:
+        self.assertEqual(self.__4.pop(3), 1)
+        self.assertEqual(str(self.__4), '❬4❭➞❬3❭➞❬2❭➞∅')
+
+    def testPop_4_n1(self) -> None:
+        self.assertEqual(self.__4.pop(), 1)
+        self.assertEqual(str(self.__4), '❬4❭➞❬3❭➞❬2❭➞∅')
+
+    def testPop_4_n4(self) -> None:
+        self.assertEqual(self.__4.pop(-4), 4)
+        self.assertEqual(str(self.__4), '❬3❭➞❬2❭➞❬1❭➞∅')
+
+    def testPop_4_2(self) -> None:
+        self.assertEqual(self.__4.pop(2), 2)
+        self.assertEqual(str(self.__4), '❬4❭➞❬3❭➞❬1❭➞∅')
+
+    def testPop_4_n3(self) -> None:
+        self.assertEqual(self.__4.pop(-3), 3)
+        self.assertEqual(str(self.__4), '❬4❭➞❬2❭➞❬1❭➞∅')
+
+
+if __name__ == '__main__':
+    unittest.main()
+
